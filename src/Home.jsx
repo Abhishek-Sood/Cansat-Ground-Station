94e2a583a5bb2518cc09c305e177ad9a326e02d3
Rohitha Pudu <rohithapudu@gmail.com>
2024-05-14 10:53:11 +0530

    add comments in src/Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,35 +1,40 @@
-
 import * as React from "react";
-import {useNavigate} from 'react-router-dom';
-import './Home.css'
+import {useNavigate} from 'react-router-dom';// Importing useNavigate hook from react-router-dom
+import './Home.css'// Importing CSS file for styling
 
-function Home() {
-  const navigate=useNavigate();
+function Home() {// Declaring Home component
+  const navigate=useNavigate(); // Initializing navigate function using useNavigate hook
+   // Function to navigate to the '/station' route
   const Station=()=>{
     navigate('/station')
   }
+  // Returning JSX for Home component
   return (
     <>
       <div className="div">
         <div className="div-2">
           <div>
-        <iframe src='https://my.spline.design/earthdayandnight-709f63feb79a009baf5af739496e348e/'
-        frameBorder='0'
-        width='100px' 
-        height='600px'
-        className='img'
-        >
-          </iframe>
+            {/* Globe animation */}
+            <iframe src='https://my.spline.design/earthdayandnight-709f63feb79a009baf5af739496e348e/'
+            frameBorder='0'
+            width='100px' 
+            height='600px'
+            className='img'
+            >
+            </iframe>
           </div>
           <div className="div-3">
+             {/* Logo image */}
             <div>
             <img src="gagan.png" alt="Logo" className="div-4"/>
             </div>
             <div className="div-5">
+               {/* Title */}
               Empowering Exploration: Open Source Ground Station for Cansat
               Satellites
             </div>
             <div>
+               {/* Button to navigate to the '/station' route */}
               <button className="div-6" onClick={Station}>
                 <div className="div-7">View</div>
               </button>
@@ -43,4 +48,4 @@ function Home() {
 }
 
 
-export default Home;
+export default Home;// Exporting Home component
