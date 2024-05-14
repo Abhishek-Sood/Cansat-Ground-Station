cd7bfeefd99babceb854b007a856798c7552d9a5
Rohitha Pudu <rohithapudu@gmail.com>
2024-05-14 14:43:53 +0530

    update src/App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
     <>
       <BrowserRouter> 
         <Routes>
-          <Route path='/station' element={<Station />} />
+          <Route path='/Station' element={<Station />} />
           <Route path='/' element={<Home />} />
         </Routes>
       </BrowserRouter>
