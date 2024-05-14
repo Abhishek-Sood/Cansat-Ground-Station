8d4a1d4075f11620f34e5953caf7557a7d01add1
Rohitha Pudu <rohithapudu@gmail.com>
2024-05-14 10:44:19 +0530

    import  src/Home.css file into src/Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 
 import * as React from "react";
 import {useNavigate} from 'react-router-dom';
+import './Home.css'
 
 function Home() {
   const navigate=useNavigate();
@@ -36,94 +37,7 @@ function Home() {
           </div>
         </div>
       </div>
-      <style jsx>{`
-        
-        .div {
-          background-color: #000;
-          display: flex;
-          flex-direction: column;
-          font-size: 26px;
-          font-weight: 400;
-        }
-        .div-2 {
-          disply: flex;
-          flex-direction: column;
-          overflow: hidden;
-          position: relative;
-          display: flex;
-          min-height: 713px;
-          // margin-top: 39px;
-          width: 100%;
-          align-items: start;
-          justify-content: center;
-          padding: 80px 60px;
-        }
-        @media (max-width: 991px) {
-          .div-2 {
-            max-width: 100%;
-            padding: 0 20px;
-          }
-        }
-        .img {
-          position: absolute;
-          inset: 0;
-          height: 100%;
-          width: 100%;
-          object-fit: cover;
-          margin-left:280px;
-          
-        }
-        .div-3 {
-          position: relative;
-          display: flex;
-          width: 568px;
-          max-width: 100%;
-          flex-direction: column;
-          margin: 23px 0 155px 55px;
-        }
-        @media (max-width: 991px) {
-          .div-3 {
-            margin-bottom: 40px;
-          }
-        }
-        .div-4 {
-          width:400px;
-        }
-        @media (max-width: 991px) {
-          .div-4 {
-            max-width: 100%;
-            font-size: 40px;
-          }
-        }
-        .div-5 {
-          color: #707070;
-          font-family: Roboto, sans-serif;
-          margin-top: 20px;
-          font-size:20px;
-        }
-        @media (max-width: 991px) {
-          .div-5 {
-            max-width: 100%;
-            margin-top: 40px;
-          }
-        }
-        .div-6 {
-         width:200px;
-         height:50px;
-         margin-top:40px;
-         cursor:pointer;
-          
-        }
-        
-        
-        @media (max-width: 991px) {
-          .div-6 {
-            margin-top: 40px;
-            white-space: initial;
-            padding: 0 20px;
-          }
-        }
-      `}</style>
+      
     </>
   );
 }
