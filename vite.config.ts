869ab372e19c984564bba7cdf9d69d68700a4753
NaunihalSingh40 <ns906227@gmail.com>
2024-05-16 22:10:37 +0530

    transformed from web to electron by Naunihal Singh

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import { UserConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()] as UserConfig["plugins"],
+  define: {
+    'process.env.SOME_KEY': JSON.stringify(loadEnv.SOME_KEY)
+  },
+  server: {
+    port: 3000
+  },
+  plugins: [react(), svgr()],
   base: "",
+  
 });
