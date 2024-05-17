b090e8a81692d519f95ad8f2d7a64ccf8cdacbb5
Witty-Wizard <shashankmarch27@gmail.com>
2024-05-17 09:38:33 +0530

    made the window resizable

--- electron/electron.js
+++ electron/electron.js
@@ -1,49 +1,47 @@
-const path = require('path');
-const { app, BrowserWindow } = require('electron');
- 
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+
 const isDev = process.env.IS_DEV == "true" ? true : false;
- 
+
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 1024,
     height: 650,
     autoHideMenuBar: true,
-    resizable: false,
+    resizable: true,
     frame: true,
     webPreferences: {
-      preload: path.join('./preload.js', 'preload.js'),
+      preload: path.join("./preload.js", "preload.js"),
       nodeIntegration: true,
-      contextIsolation: false
+      contextIsolation: false,
     },
   });
- 
+
   mainWindow.webContents.setWindowOpenHandler((edata) => {
     shell.openExternal(edata.url);
     return { action: "deny" };
   });
- 
+
   mainWindow.loadURL(
     isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join('../index.html', '../dist/index.html')}`
+      ? "http://localhost:3000"
+      : `file://${path.join("../index.html", "../dist/index.html")}`
   );
   // Open the DevTools.
   if (isDev) {
-    //mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
   }
- 
 }
- 
- 
+
 app.whenReady().then(() => {
-  createWindow()
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+  createWindow();
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
- 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
